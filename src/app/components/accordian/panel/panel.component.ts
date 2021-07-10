c78314b9f79459bfe94bdee5549b46f73dcff411
Anil G R <anil@foradian.com>
2021-07-10 14:24:52 +0530

    feat: accordian and panel component added.

---
+++ src/app/components/accordian/panel/panel.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss'],
+})
+export class PanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  show() {
+
+  }
+
+  hide() {
+
+  }
+
+  toggle() {
+
+  }
+
+}
