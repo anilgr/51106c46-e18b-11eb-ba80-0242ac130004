a7a37f74271e39a458db0d5d161bcc0e9f7b7e49
Anil G R <anil@foradian.com>
2021-07-10 15:16:04 +0530

    feat: scorecard page panels header content added.

--- src/app/scorecard/scorecard.module.ts
+++ src/app/scorecard/scorecard.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ScorecardPageRoutingModule } from './scorecard-routing.module';
 
 import { ScorecardPage } from './scorecard.page';
+import { ComponentModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ScorecardPageRoutingModule
+    ScorecardPageRoutingModule,
+    ComponentModule
   ],
   declarations: [ScorecardPage]
 })
--- src/app/scorecard/scorecard.page.ts
+++ src/app/scorecard/scorecard.page.ts
@@ -7,12 +7,15 @@ import { ScorecardService } from '../services/scorecard/scorecard.service';
   styleUrls: ['./scorecard.page.scss'],
 })
 export class ScorecardPage implements OnInit {
-
+  scorecards: any[] = []
   constructor(private scorecardService: ScorecardService) { }
 
   ngOnInit() {
     this.scorecardService.getScorecardData().subscribe((res)=>{
       console.log(res)
+      if(res.success) {
+        this.scorecards = res.data.scorecard;
+      }
     })
   }
 
