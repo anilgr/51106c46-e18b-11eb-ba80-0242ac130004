1c945c5bb61aa96bfe76d59dcf734bc899f6c398
Anil G R <anil@foradian.com>
2021-07-10 14:25:21 +0530

    feat: app module update.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AccordianComponent } from './components/accordian/accordian.component';
+import { PanelComponent } from './components/accordian/panel/panel.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AccordianComponent, PanelComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
