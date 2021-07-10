dada7d95da718c71b75f6180215b4915d1b7d890
Anil G R <anil@foradian.com>
2021-07-10 15:13:37 +0530

    feat: common module added to components module.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AccordianComponent } from "./accordian/accordian.component";
 import { PanelComponent } from "./accordian/panel/panel.component";
 
 @NgModule({
     declarations: [AccordianComponent, PanelComponent],
+    imports: [CommonModule],
     exports: [AccordianComponent, PanelComponent]
 })
 export class ComponentModule {
-    
+
 }
